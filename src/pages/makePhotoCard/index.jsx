745c6429a9382b0e9724b227765ce4b79636ee6e
shinhye ha <mandely01@gmail.com>
2025-12-16 22:50:49 +0000

    error modified

--- src/pages/makePhotoCard/index.jsx
+++ src/pages/makePhotoCard/index.jsx
@@ -113,7 +113,7 @@ export default function MakePhotoCard() {
 
       <form onSubmit={handleSubmit(onSubmit)} className="mt-14 flex flex-col items-center gap-14">
         <div className={divStyle}>
-          <label className={labelStyle}>Photo Card's Name</label>
+          <label className={labelStyle}>Name of Card</label>
           <Input
             {...register('name', {
               required: 'This field is required.',
--- src/pages/makePhotoCard/success.jsx
+++ src/pages/makePhotoCard/success.jsx
@@ -1,26 +1,22 @@
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import SecondaryButton from '@/components/shared/SecondaryButton';
 
-const goToMyGallery = () => {
-  const router = useRouter();
-  router.push('/mygallery');
-}
-
 export default function Success() {
+  const router = useRouter();
+
+  const goToMyGallery = () => {
+    router.push('/mygallery');
+  };
+
   return (
     <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-9">
-      {/* <button className="relative top-[-110px] right-[-300px]">
-        <Image src="/images/type=close.png" width={24} height={24} />
-      </button> */}{' '}
-      {/* 버튼이 필요한가? */}
       <h1 className="font-baskin text-[46px] font-normal leading-[47px]">
         Make Photo Card <span className="text-customMain">Success</span>
       </h1>
       <p className="text-xl font-bold leading-7">Success!</p>
       <SecondaryButton
-        label={'Go to My Gallery'}
-        className={'h-[60px] w-[440px] border-white'}
+        label="Go to My Gallery"
+        className="h-[60px] w-[440px] border-white"
         handleClick={goToMyGallery}
       />
     </div>
